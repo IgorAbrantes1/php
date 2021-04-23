db3b98e4e7a9ae2f91364bc14a47e9f5f5be6e81
Igor Abrantes <igorlk109@hotmail.com>
2021-04-23 19:20:29 -0300

    primeira módulo completo

---
+++ meus-arquivos/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+    <!-- Fonte -->
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/base.css" />
+
+    <!-- Título -->
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="header">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="main">
+        <div class="container">
+            <nav class="modules">
+                <div class="module verde">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=teste&file=teste">Exercício A</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="footer">COD3R & IGOR ABRANTES © <?= date('Y'); ?></footer>
+</body>
+
+</html>
