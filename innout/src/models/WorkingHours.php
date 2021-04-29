d13bbe34fc91625990e19dfbf04b529346470fb9
Igor Abrantes <igorlk109@hotmail.com>
2021-04-29 20:00:01 -0300

    Relatório Mensal

--- innout/src/models/WorkingHours.php
+++ innout/src/models/WorkingHours.php
@@ -29,6 +29,36 @@ class WorkingHours extends Model
         return $registry;
     }
 
+    function getBalance()
+    {
+        if (!$this->time1 && !isPastWorkDay($this->work_date)) return '';
+        if($this->worked_time == DAILY_TIME) return '---';
+
+        $balance = $this->worked_time - DAILY_TIME;
+        $balanceString = getTimeStringFromSeconds(abs($balance));
+        $sign = $this->worked_time >= DAILY_TIME ? '+' : '-';
+        return "{$sign}{$balanceString}";
+    }
+
+    public static function getMonthlyReport($userId, $date)
+    {
+        $registries = [];
+        $startDate = getFirstDayOfMonth($date)->format('Y-m-d');
+        $endDate = getLastDayOfMonth($date)->format('Y-m-d');
+
+        $result = static::getResultSetFromSelect([
+            'user_id' => $userId,
+            'raw' => "work_date between '{$startDate}' AND '{$endDate}'"
+        ]);
+
+        if ($result) {
+            while ($row = $result->fetch_assoc()) {
+                $registries[$row['work_date']] = new WorkingHours($row);
+            }
+        }
+        return $registries;
+    }
+
     public function getActiveClock()
     {
         $nextTime = $this->getNextTime();
@@ -57,6 +87,7 @@ class WorkingHours extends Model
             throw new AppException("Você já fez os 4 batimentos do dia!");
         }
         $this->$timeColumn = $time;
+        $this->worked_time = getSecondsFromDateInterval($this->getWorkedInterval());
 
         if ($this->id) {
             $this->update();
--- innout/src/views/template/header.php
+++ innout/src/views/template/header.php
@@ -7,42 +7,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="assets/css/comum.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/icofont.min.css">
     <link rel="stylesheet" href="assets/css/template.css">
     <title>In N' Out</title>
 </head>
 
 <body>
-    <header class="header">
-        <div class="logo">
-            <i class="icofont-travelling mr-2"></i>
-            <span class="font-weight-light">In </span>
-            <span class="font-weight-bold mx-2"> N' </span>
-            <span class="font-weight-light"> Out </span>
-            <i class="icofont-runner-alt-1 ml-2"></i>
-        </div>
-        <div class="menu-toggle mx-3">
-            <i class="icofont-navigation-menu"></i>
-        </div>
-        <div class="spacer"></div>
-        <div class="dropdown">
-            <div class="dropdown-button">
-                <img class="avatar" src="<?= "http://www.gravatar.com/avatar.php?gravatar_id=" . md5(strtolower(trim($_SESSION['user']->email))) ?>" alt="user">
-                <span class="mx-1">
+<header class="header">
+    <div class="logo">
+        <i class="icofont-travelling mr-2"></i>
+        <span class="font-weight-light">In </span>
+        <span class="font-weight-bold mx-2"> N' </span>
+        <span class="font-weight-light"> Out </span>
+        <i class="icofont-runner-alt-1 ml-2"></i>
+    </div>
+    <div class="menu-toggle mx-3">
+        <i class="icofont-navigation-menu"></i>
+    </div>
+    <div class="spacer"></div>
+    <div class="dropdown">
+        <div class="dropdown-button">
+            <img class="avatar"
+                 src="<?= "http://www.gravatar.com/avatar.php?gravatar_id=" . md5(strtolower(trim($_SESSION['user']->email))) ?>"
+                 alt="user">
+            <span class="mx-1">
                     <?= $_SESSION['user']->name ?>
                 </span>
-                <i class="icofont-simple-down ml-3"></i>
-            </div>
-            <div class="dropdown-content">
-                <ul class="nav-list">
-                    <li class="nav-item">
-                        <a href="logout.php">
-                            <i class="icofont-logout mr-2"></i>
-                            Sair
-                        </a>
-                    </li>
-                </ul>
-            </div>
+            <i class="icofont-simple-down ml-3"></i>
         </div>
-    </header>
+        <div class="dropdown-content">
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="logout.php">
+                        <i class="icofont-logout mr-2"></i>
+                        Sair
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
